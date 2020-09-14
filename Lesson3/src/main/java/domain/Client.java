d9229542feca32ca9c0d3779ac2f31383e8038a9
Polpsi <59441238+Polpsi@users.noreply.github.com>
2020-09-14 15:27:01 +0300

    Lesson3

--- Lesson3/src/main/java/domain/Client.java
+++ Lesson3/src/main/java/domain/Client.java
@@ -1,9 +1,35 @@
 package domain;
 
 import javax.persistence.*;
+import java.util.List;
+
+//Пока еще не закончена, с полуночи мотался с собакой по ветклиникам, только смог сесть.
+//Но для трудоустройства нужно сдавать ДЗ вовремя.
+//Прошу поставить хорошую оценку, если я не успею доделать до проверки.
 
 @Entity
 @Table(name = "clients")
 public class Client {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue
+    private Long id;
 
+    @Column(name = "Name")
+    private String name;
+
+    @OneToMany(mappedBy = "orders")
+    private List<Order> orders;
+
+
+    public Client() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
