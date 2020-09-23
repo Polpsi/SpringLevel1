4d3a0c128fc73c4891079a65a983b87a66b50d9d
Polpsi <59441238+Polpsi@users.noreply.github.com>
2020-09-23 23:22:40 +0300

    Lesson6Before

---
+++ Lesson6/src/main/java/Lesson6/controller/MainController.java
@@ -0,0 +1,15 @@
+package Lesson6.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("testtext", "fdsa");
+        return "index";
+    }
+}
