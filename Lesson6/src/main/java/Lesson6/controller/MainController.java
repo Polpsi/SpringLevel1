8c784afebcd0e0382548e7659f2ece2674965ff5
Polpsi <59441238+Polpsi@users.noreply.github.com>
2020-09-24 02:55:19 +0300

    Lesson6

--- Lesson6/src/main/java/Lesson6/controller/MainController.java
+++ Lesson6/src/main/java/Lesson6/controller/MainController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    // Получить список продуктов, посмотреть продукт детально,
+    // добавить в базу и удалить продукты из базы
+
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("testtext", "fdsa");
